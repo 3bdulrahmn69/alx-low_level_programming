fb2c44fa6db68814f4c4599af2b68a881df44f0e
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-16 02:21:01 -0700

    4.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-char c[] = "Programming is like building a multilingual puzzle";
+char c[] = "\"Programming is like building a multilingual puzzle";
 puts(c);
 return (0);
 }
