14600efde073ff324bed7e92b365fbbf8ade29be
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-16 05:12:32 -0700

    adv

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <srdio.h>
+/**
+ * main - heye there
+ * Return: always 0 (succses)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
