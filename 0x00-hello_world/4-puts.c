74f3fc6e92e71da82d77a4e59ac184e9eacfe206
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-16 02:59:04 -0700

    4.4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Succse)
+ */
 int main(void)
 {
 char c[] = "\"Programming is like building a multilingual puzzle";
