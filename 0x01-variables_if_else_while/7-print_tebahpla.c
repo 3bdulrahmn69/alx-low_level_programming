531e0b33c475a255b997b0fcc97d9f235552482f
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-17 09:01:29 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - no no no
+ * Return: hey ss
+ */
+int main(void)
+{
+	char ec;
+
+	for (ec = 'z' ; ec >= 'a' ; ec--)
+	{
+		putchar(ec);
+	}
+	putchar('\n');
+	return (0);
+}
