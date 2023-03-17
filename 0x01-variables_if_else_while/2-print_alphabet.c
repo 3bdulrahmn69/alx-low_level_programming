a2b6cb3be2d20e79d33469d7be377aa4a037069f
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-17 07:30:13 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - hey main
+ * Retutn: baby shark
+ */
+int main(void)
+{
+	char alph;
+
+	for(alph = a ; alph <= z ; a++)
+	{
+		putchar(alph);
+		putchar("\n");
+	}
+	return (0);
+}
