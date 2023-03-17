137b6a713a5e43f31e595e0fd3a8750c5a2d006f
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-17 09:10:48 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - no no no
+ * Return: hey ss
+ */
+int main(void)
+{
+	int i;
+	char za;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	for (za = 'e' ; i < 'f' ; za++)
+	{
+		putchar(za);
+	}
+	putchar('\n');
+	return (0);
+}
