1b933ee6d1711ce953dd80cb735ba883a9045cc4
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 07:29:41 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Program enter point
+ * Return: Program return
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	while (i <= 10)
+	{
+		for (a = 'a' ; a <= 'z' ; a++)
+			_putchar(a);
+		i++
+	}
+	_putchar('\n');
+}
