1b444602c8dfed7bfbedc595f5b96c892719407f
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 10:23:47 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
- * main - Program enter point
+ * jack_bauer - Program enter point
  * Return: Program return
  */
 void jack_bauer(void)
 {
 	int h, m;
 
-	for(h = 0; h <= 23 ; h++)
+	for (h = 0; h <= 23 ; h++)
 	{
-		for(m = 0; m <=59 ; m++)
+		for (m = 0; m <= 59 ; m++)
 		{
+			if (h < 10)
+				_putchar('0');
 			_putchar(h + '0');
 			_putchar(':');
+			if (m < 10)
+				_putchar('0');
 			_putchar(m + '0');
-			_putchar('\n');
 		}
 	}
 }
