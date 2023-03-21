3879a030ce064548c1a1e065572cb49b11d774a2
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 09:45:45 -0700

    ss

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,7 @@ int print_last_digit(int n)
 	int k;
 
 	k = n % 10;
-	if (n < 0)
-		k = -k;
+
 	_putchar(k + '0');
 	return (k);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main - Program enter point
+ * Return: Program return
+ */
+void jack_bauer(void)
+{
+	
+}
