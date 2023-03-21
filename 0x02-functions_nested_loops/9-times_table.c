35d725bd56215a6aeaa315bf7b4140494e4ca30b
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 10:43:41 -0700

    9; git push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - Program enter point
+ * Return: Program return
+ */
+void times_table(void)
+{
+	int n, m, x;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			x = n * m;
+
+			if (x <= 9)
+				_putchar(' ');
+			else
+				_putchar((x / 10) + '0');
+
+			_putchar((x % 10) + '0');
+		}
+		_putchar('\n');
+}
