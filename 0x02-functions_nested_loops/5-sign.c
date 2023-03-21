91931a70606571c38432b68956dc69340ff1cb1e
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 08:30:40 -0700

    4,5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign - Program enter point
+ * Return: Program 1 , 0 , -1
+ * @n: is an n
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
