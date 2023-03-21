1a21b56721e2ce4d7b8776772791fe6feab1cab9
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 07:38:55 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	char a;
 	int i;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		for (a = 'a' ; a <= 'z' ; a++)
 			_putchar(a);
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
