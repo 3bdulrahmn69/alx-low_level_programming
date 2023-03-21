e44348c3f9d5c0dcde310ceaea8588101f0c78ea
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 08:05:17 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - Program enter point
+ * Return: Program return
+ * @c: is a c
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
