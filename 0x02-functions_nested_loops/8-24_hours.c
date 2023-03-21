ce91490aa758ad9ebf4f8632352a2949f3bcab24
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-21 10:16:48 -0700

    test

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,5 +5,16 @@
  */
 void jack_bauer(void)
 {
-	
+	int h, m;
+
+	for(h = 0; h <= 23 ; h++)
+	{
+		for(m = 0; m <=59 ; m++)
+		{
+			_putchar(h + '0');
+			_putchar(':');
+			_putchar(m + '0');
+			_putchar('\n');
+		}
+	}
 }
