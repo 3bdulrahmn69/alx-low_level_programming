fa3a49a5a049bd5b242f3a04a2dceabc22a2b27a
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-23 05:22:56 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ * Return: only print
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		if (i != 4 || i != 2)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
