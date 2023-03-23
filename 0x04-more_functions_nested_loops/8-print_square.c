f61aa388a3233c71c7d6ad547e8aa40d125446a3
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-23 08:12:01 -0700

    hey square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * Return: square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int i, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		for (k = 0 ; k <= size ; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
