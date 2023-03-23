d9abb49051aa8fc3e5faae350f18d4f2f5084ed3
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-23 04:00:10 -0700

    first commit today :D

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - fun that make fun
+ * Description: 'the program's description
+ * @c: is an cCc
+ * Return: 1 , 0
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+		return (1);
+	else
+		return (0);
+}
