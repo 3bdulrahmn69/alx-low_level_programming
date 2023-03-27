b7254be2727938a23b87bba04bcea62796507128
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-27 10:27:24 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - hey there
+ * @dest: an dest
+ * @src: an src
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
