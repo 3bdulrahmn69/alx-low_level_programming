8dbd92b8ce4ab0f2f2763df4ba2cf435232f4d30
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-27 08:55:57 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - hsss
+ * @str: hsss
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0, x;
+
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	if (i + 1 % 2 != '0')
+	{
+		x = (i - 1) / 2;
+	}
+	else
+	{
+		x = (i / 2);
+	}
+	x++;
+	for (i = x ; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
