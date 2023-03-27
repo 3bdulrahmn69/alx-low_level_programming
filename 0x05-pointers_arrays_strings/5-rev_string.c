497a0b3c09b124e46a11434bebd516fbb13351be
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-27 08:16:51 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#inclde "main.h"
+
+/**
+ * rev_string - revvv
+ * @s: sss
+ * Return: voide
+ */
+void rev_string(char *s)
+{
+	int x = 0, y = 0, z;
+	char c;
+
+	while (s[x] != '\0')
+	{
+		x++
+	}
+	y = x - 1;
+	for (z = 0 ; y >= 0 && z < y ; y-- ; z++)
+	{
+		c = s[z];
+		s[z] = s[y];
+		s[y] = c;
+	}
+}
