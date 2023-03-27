c5dc4ccae40a72519294c9cae975940b30b9691b
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-03-27 07:27:24 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - helllp
+ * @s: sssssss
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	while (i-- != 0)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
