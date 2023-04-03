906934c733063ec6b7ea61fbd1cc91a8a6776ee4
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-03 06:45:06 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,8 +5,15 @@
  * @s: char
  * @b: char
  * @n: int
- * Return: char 
+ * Return: char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i;
+
+	for (i = 0 ; n > 0 ; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
