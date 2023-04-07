0782d1b3b50d074ccfbbb87f279e872dd0c45c8f
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-07 12:18:20 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - main
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
+	return (0);
+}
