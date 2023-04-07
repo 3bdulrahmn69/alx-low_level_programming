8186b52411d05eb57893311c0ef3369eea996030
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-07 12:44:33 -0700

    add

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,8 +15,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
+	}
 	return (0);
 }
