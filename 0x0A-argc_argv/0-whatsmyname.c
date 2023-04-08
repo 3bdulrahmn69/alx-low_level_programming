ddac26cacc31dacf2614025534e3e40b1de61d94
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-08 06:39:56 -0700

    IDK

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,8 @@
  * @argv: char
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	(void)argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
