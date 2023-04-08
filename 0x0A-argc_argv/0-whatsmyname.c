412ab83344c5a552427ac5c12785a01a3b303701
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-08 06:42:55 -0700

    !!!!!!!!!!!

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,9 @@
  * @argv: char
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void)argc;
+	printf("%s\n", *argv);
 
-	printf("%s\n", argv[0]);
 	return (0);
 }
