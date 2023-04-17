f54ac84d57b1bb3cbf33a7686063b85f05c4adad
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-17 11:38:05 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog
+ * @name: dog Name
+ * @age: dog Age
+ * owner: dog Owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
