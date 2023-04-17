0499447718cff28713d0c471e1fa245b46440fb8
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-17 11:50:52 -0700

    hh

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,17 @@ struct dog
 	char *owner;
 };
 
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+
+typedef struct dog dog_t;
+
 #endif
