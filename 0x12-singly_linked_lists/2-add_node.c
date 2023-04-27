b2cfc88fafb492d70abb9f36822b8b03565ff4a2
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-04-27 10:31:21 -0700

    3e task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * _strlen - function that count len
+ * @str: string
+ * Return: len
+ */
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list
+ * @head: list
+ * @str: char
+ * Return:the address of the new element, or NULL if it failed
+ */
+ist_t *add_node(list_t **head, const char *str)
+{
+	list_t *add;
+
+	add = malloc(sizeof(list_t));
+	if (add == NULL)
+	{
+		return (NULL);
+	}
+	add->str = strdup(str);
+	add->len = _strlen(str);
+	add->next = *head;
+	*head = add;
+	return (add);
+}
