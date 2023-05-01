59a37dc7a8f8972fcc3a0a86ba0343638dbfe4a3
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-05-01 13:25:53 -0700

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,8 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		new_node = *head;
-		return (new_node);
+		return (add_nodeint(head, n));
 	}
 	current_node = *head;
 	for (i = 0; i < idx - 1; i++)
