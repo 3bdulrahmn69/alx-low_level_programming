8471480c417001ba345650fe26d80374cf2197cd
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-05-08 11:01:33 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,28 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
+	int fh_from = open(argv[1], O_RDONLY);
+	int fh_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	int fh_from = open(argv[1], O_RDONLY);
 
 	if (fh_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	int fh_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
 	if (fh_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read;
-
 	while ((bytes_read = read(fh_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		ssize_t bytes_written = write(fh_to, buffer, bytes_read);
