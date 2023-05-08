62b37d3515344ddb731d1bcfbd9fdf6993b76bea
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-05-08 07:56:40 -0700

    tast

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 		/* Prototypes */
 int _putchar(char c);
